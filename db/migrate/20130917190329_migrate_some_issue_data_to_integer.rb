858d339385fed42bd4a570b3a6d8920ae33513b0
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-18 16:10:02 +0200

    Change migration for migrating string to integer
    Change SQL because it was not working on heroku.

--- db/migrate/20130917190329_migrate_some_issue_data_to_integer.rb
+++ db/migrate/20130917190329_migrate_some_issue_data_to_integer.rb
@@ -1,9 +1,9 @@
 class MigrateSomeIssueDataToInteger < ActiveRecord::Migration
   def up
-    connection.execute 'ALTER TABLE issues ALTER COLUMN smt_limit TYPE INTEGER USING CAST(smt_limit AS INTEGER)'
-    connection.execute 'ALTER TABLE issues ALTER COLUMN solvers_limit TYPE INTEGER USING CAST(solvers_limit AS INTEGER)'
-    connection.execute 'ALTER TABLE issues ALTER COLUMN fake_smts TYPE INTEGER USING CAST(fake_smts AS INTEGER)'
-    connection.execute 'ALTER TABLE issues ALTER COLUMN fake_solvers TYPE INTEGER USING CAST(fake_solvers AS INTEGER)'
+    connection.execute 'ALTER TABLE issues ALTER COLUMN smt_limit TYPE INTEGER USING smt_limit::INTEGER'
+    connection.execute 'ALTER TABLE issues ALTER COLUMN solvers_limit TYPE INTEGER USING solvers_limit::INTEGER'
+    connection.execute 'ALTER TABLE issues ALTER COLUMN fake_smts TYPE INTEGER USING fake_smts::INTEGER'
+    connection.execute 'ALTER TABLE issues ALTER COLUMN fake_solvers TYPE INTEGER USING fake_solvers::INTEGER'
   end
 
   def down
