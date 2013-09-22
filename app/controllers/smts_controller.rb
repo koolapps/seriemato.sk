7195e6c83ef4fa87f2d49dbb8572d3868096cb64
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-22 15:22:19 +0200

    Save SMT user data in cookies + fix SMT user data bug (email not saved)

--- app/controllers/smts_controller.rb
+++ app/controllers/smts_controller.rb
@@ -3,12 +3,14 @@ class SmtsController < ApplicationController
 
   def new
     @smt = issue.smts.build
+    restore_user_data_from_cookies
   end
 
   def create
     @smt = issue.smts.build(smt_params)
     if params[:commit] == SAVE_DATA_AND_VOTE
       @smt.validate = true
+      save_user_data_to_cookies
     end
     if @smt.save
       flash[:success] = 'Ďakujeme! Váš hlas bol započítaný.'
@@ -26,6 +28,22 @@ class SmtsController < ApplicationController
   def smt_params
     params.
       require(:smt).
-      permit(:sex, :year_of_birth, :city, :job)
+      permit(:sex, :year_of_birth, :city, :job, :email)
+  end
+
+  def save_user_data_to_cookies
+    cookies[:smt_sex] = { value: smt_params[:sex], expires: 3.months.from_now }
+    cookies[:smt_year_of_birth] = { value: smt_params[:year_of_birth], expires: 3.months.from_now }
+    cookies[:smt_job] = { value: smt_params[:job], expires: 3.months.from_now }
+    cookies[:smt_city] = { value: smt_params[:city], expires: 3.months.from_now }
+    cookies[:smt_email] = { value: smt_params[:email], expires: 3.months.from_now }
+  end
+
+  def restore_user_data_from_cookies
+    @smt.sex = cookies[:smt_sex]
+    @smt.year_of_birth = cookies[:smt_year_of_birth]
+    @smt.job = cookies[:smt_job]
+    @smt.city = cookies[:smt_city]
+    @smt.email = cookies[:smt_email]
   end
 end
--- spec/features/issue_page_spec.rb
+++ spec/features/issue_page_spec.rb
@@ -49,6 +49,32 @@ feature 'Issue page' do
     expect(smt.year_of_birth).to eq '1990'
     expect(smt.city).to eq 'Košice I'
     expect(smt.job).to eq 'Služby'
+    expect(smt.email).to eq 'user@example.com'
+  end
+
+  scenario 'click on SMT button and enter user data saves user data in cookies', js: true do
+    issue = FactoryGirl.create(:issue)
+    another_issue = FactoryGirl.create(:issue)
+    visit issue_path(issue)
+    click_link 'Serie ma to!'
+    expect(page).to have_css 'h4', text: 'Kto ste? Čo ste?'
+    choose 'Žena'
+    choose 'Muž'
+    select '1990', from: 'Rok narodenia'
+    select 'Košice I', from: 'Okres'
+    select 'Služby', from: 'Zamestnanie'
+    fill_in 'Email', with: 'user@example.com'
+    click_button 'Uložiť údaje a hlasovať'
+    sleep 1
+    visit issue_path(another_issue)
+    click_link 'Serie ma to!'
+    within '#modal' do
+      expect(page).to have_css 'input[value="Muž"][checked="checked"]'
+      expect(page).to have_css 'option[value="1990"][selected="selected"]'
+      expect(page).to have_css 'option[value="Košice I"][selected="selected"]'
+      expect(page).to have_css 'option[value="Služby"][selected="selected"]'
+      expect(page).to have_css 'input[value="user@example.com"]'
+    end
   end
 
   scenario 'click on SMT buttong and send empty user data', js: true do
@@ -62,4 +88,5 @@ feature 'Issue page' do
   end
 
   scenario 'click on SMT button increase SMT count just once for each user'
+
 end
