857113dda0510a9eb97b35ebd291db2fc74d9c38
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-25 16:59:11 +0200

    Change formatting in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Seriemato::Application.routes.draw do
   root to: 'index_pages#show'
+  get '/admin', to: 'admin/issues#index'
+
   namespace :admin do
-    resources :issues, only: [:new, :create, :edit, :index, :update]
     resources :categories, only: [:new, :create, :edit, :update, :index, :destroy]
+    resources :issues, only: [:new, :create, :edit, :index, :update]
   end
+
   resources :issues, only: [:show, :index] do
     resources :smts, only: [:new, :create]
     resources :solvers, only: [:new, :create]
   end
-  resources :smts, only: [:update]
+
   resources :category, only: [:show]
-  get '/admin', to: 'admin/issues#index'
+  resources :smts, only: [:update]
 end
