bb41e6ae78e60f30775672ddd385b0f95049dfce
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-25 16:46:05 +0200

    Change order of valides method calls

--- app/models/solver.rb
+++ app/models/solver.rb
@@ -1,5 +1,5 @@
 class Solver < ActiveRecord::Base
   belongs_to :issue
-  validates :first_name, :last_name, :email, presence: true
   validates :email, email: true
+  validates :first_name, :last_name, :email, presence: true
 end
