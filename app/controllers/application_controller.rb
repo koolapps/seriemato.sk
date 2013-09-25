c29fb7637e3bf960fa00a394d1a631d6e9391e1d
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-25 15:49:34 +0200

    Move footer categories out of the application controller
    Data are now loaded directly from layout. Maybe it is not the bast
    solution, but it is better than having unnecessary instance variable in
    all controller actions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_filter :set_footer_categories
-
-  private
-
-  def set_footer_categories
-    @footer_categories = Category.all
-  end
 end
