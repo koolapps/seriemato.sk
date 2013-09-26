5cdbea6b56bb36d3208faef4cdc0bbb8299bad0e
Patrik Bóna <patrik.bona@mrhead.sk>
2013-09-26 15:22:37 +0200

    Add progress bars to index page

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -26,13 +26,25 @@ feature 'Index page' do
   end
 
   scenario 'has all needed elements for random issue' do
-    issue = FactoryGirl.create(:issue, name: 'My Issue', short_description: 'This is my issue', picture: picture_file('large_picture.jpg'))
+    issue = FactoryGirl.create(:issue,
+                               name: 'My Issue',
+                               short_description: 'This is my issue',
+                               picture: picture_file('large_picture.jpg'),
+                               fake_solvers: 2,
+                               solvers_limit: 10,
+                               fake_smts: 1,
+                               smt_limit: 10
+                              )
     visit root_path
     within "div#issue_#{issue.id}" do
       expect(page).to have_css 'h3', text: 'My Issue'
       expect(page).to have_css 'p', text: 'This is my issue'
       expect(page).to have_css 'a img[src*="large_picture.jpg"]'
       expect(page).to have_link issue.name, href: issue_path(issue)
+      expect(page).to have_css '.smt-count', text: 'Serie to: 1 / 10'
+      expect(page).to have_css '.solvers-count', text: 'Chce to riešiť: 2 / 10'
+      expect(page).to have_css '.progress-bar.smt', text: '10% Hotovo'
+      expect(page).to have_css '.progress-bar.solver', text: '20% Hotovo'
     end
   end
 
