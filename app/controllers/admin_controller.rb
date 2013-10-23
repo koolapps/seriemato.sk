3c86201b8171014f6b2b9f93819c0666e6a86db6
Patrik Bóna <patrik.bona@mrhead.sk>
2013-10-23 14:37:49 +0200

    Move authentication to admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,13 @@
 class AdminController < ApplicationController
   layout 'admin'
+
+  before_filter :authenticate if Rails.env.production?
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['login'] && password == ENV['password']
+    end
+  end
 end
